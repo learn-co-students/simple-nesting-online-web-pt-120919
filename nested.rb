e5f6366ba5d995bdacb8d6471c74d7eb46ff4c24
Allen Phillip Pottinger <allenpottinger@gmail.com>
2019-12-21 19:53:18 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,11 +16,13 @@ def hopper
         }
      }
 
-
+programmer_hash[:languages_techniques] = "Nested Hashes"
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	programmer_hash[:alan_key][:known_for]
+	
 	
 	programmer_hash = 
  		{
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -79,6 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
+  programmer_hash[:yukihiro_matsumoto] => [:known_for => "Ruby", :languages => ["LISP", "C"]
 
     
 end
@@ -101,7 +105,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = {#alans_new_info}
      
 end
 
@@ -121,5 +125,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie] = "likes"
 end
